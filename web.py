59bde31c93d705693ed6073c517874a666d2d630
Thomas Alfred Troiano Carlsen <Thomas Alfred Troiano Carlsen>
2016-10-05 16:46:31 -0400

    Created an about page and added a Bootstrap

--- web.py
+++ web.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about_page.html')
+
 app.run(debug=True)
 
 
